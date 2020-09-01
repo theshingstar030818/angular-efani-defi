b064f57221f2441ad8d7138c0226f60f605ce4bd
Tanzeel Rehman <tanzeel.rehman@solarwinds.com>
2020-09-01 14:34:34 -0400

    add parse server

--- parse-server/start.sh
+++ parse-server/start.sh
@@ -7,7 +7,16 @@ fuser -k 1337/tcp
 fuser -k 4040/tcp
 
 mongodb-runner start
-parse-server --appId YOUR_APP_ID --masterKey YOUR_MASTER_KEY --databaseURI mongodb://localhost/dev & 
+
+parse-server --appId YOUR_APP_ID \
+--masterKey YOUR_MASTER_KEY \
+--databaseURI mongodb://localhost/dev & 
+
+parse-dashboard --dev --appId YOUR_APP_ID \
+--masterKey YOUR_MASTER_KEY \
+--serverURL "http://95.217.18.84/parse" \
+--appName EfaniGas &
+
 mongorestore --drop -d dev db/dev/ 
 
 
