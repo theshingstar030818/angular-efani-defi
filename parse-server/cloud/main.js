a9438817caf190633b0d20e5a8843656b9cc821a
Tanzeel Rehman <tanzeel.rehman@solarwinds.com>
2020-09-01 15:18:42 -0400

    added cloud code hello

--- parse-server/cloud/main.js
+++ parse-server/cloud/main.js
@@ -1,22 +1,12 @@
-Parse.Cloud.define('hello', function(request, response) {
-    var query = new Parse.Query('Subscribers');
-    query.count({ useMasterKey: true }) // count() will use the master key to bypass ACLs
-    .then(function(count) {
-        response.success(count);
-    });
-});
-
-Parse.Cloud.define("hello", async request=> {
+Parse.Cloud.define("getSubscriberCount", async request=> {
     const query = new Parse.Query("Subscribers");
-    
-
     let results;
     try{
         results = await query.count({ useMasterKey: true }); // count() will use the master key to bypass ACLs
         console.log(results);
         return results;
     } catch(error){
-        throw error.message;
+        return error.message;
     }
-
 });
+
