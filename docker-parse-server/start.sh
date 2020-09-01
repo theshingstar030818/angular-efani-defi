6c76f42b5b27ea4fc86802b5b4147e37636b8dc0
Tanzeel Rehman <tanzeel.rehman@solarwinds.com>
2020-09-01 12:07:56 -0400

    update server address

--- docker-parse-server/start.sh
+++ docker-parse-server/start.sh
@@ -8,7 +8,7 @@ APP_NAME=EfaniGas \
 APP_ID=YOUR_APP_ID \
 MASTER_KEY=YOUR_MASTER_KEY \
 PARSE_DASHBOARD_ALLOW_INSECURE_HTTP=1 \
-SERVER_URL=http://localhost:1337/parse \
+SERVER_URL=http://95.217.18.84:1337/parse \
 USER1=admin \
 USER1_PASSWORD=pass \
 docker-compose up -d
