f8948f73f4c0f25f2f7f485ac700af8f6eb84619
Tanzeel Rehman <tanzeel.rehman@solarwinds.com>
2020-09-02 07:04:41 -0400

    added twilio to parse server and inviteWithTwilio cloud code

--- parse-server/cloud/main.js
+++ parse-server/cloud/main.js
@@ -1,3 +1,20 @@
+// Include the Twilio Cloud Module and initialize it
+var twilio = require("twilio");
+twilio.initialize("AC6d140e5ebe108ae77b04f2dcb4258246", "4b390328818292afccb265aa724825eb");
+
+// Create the Cloud Function
+Parse.Cloud.define("inviteWithTwilio", function(request, response) {
+    // Use the Twilio Cloud Module to send an SMS
+    twilio.sendSMS({
+        From: "myTwilioPhoneNumber",
+        To: request.params.number,
+        Body: "Start using Parse and Twilio!"
+    }, {
+        success: function(httpResponse) { response.success("SMS sent!"); },
+        error: function(httpResponse) { response.error("Uh oh, something went wrong"); }
+    });
+});
+
 Parse.Cloud.define("getSubscriberCount", async request=> {
     const query = new Parse.Query("Subscribers");
     let results;
