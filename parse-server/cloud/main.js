2ffdf20e95e38900d2e89e6376b768d10f073071
Tanzeel Rehman <tanzeel.rehman@solarwinds.com>
2020-09-02 10:02:31 -0400

    add verify dialog and verify cloud code

--- parse-server/cloud/main.js
+++ parse-server/cloud/main.js
@@ -2,25 +2,33 @@ const accountSid = 'AC6d140e5ebe108ae77b04f2dcb4258246';
 const authToken = '4b390328818292afccb265aa724825eb';
 const twilio = require('twilio')(accountSid, authToken);
 
+const FROM = '+12037634874';
+
 Parse.Cloud.afterSave("Subscribers", async (req) => {
     var sendOnTimePin = req.context.sendOnTimePin;
     var otp = req.context.otp;
     console.log(sendOnTimePin);
     console.log(otp);
+
+    console.log(req.params);
     
     if (sendOnTimePin) {
 
     }
 });
 
-Parse.Cloud.define("inviteWithTwilio", function(request, response) {
+function sendSMS(phoneNumber, message) {
     twilio.messages
     .create({
-        body: request.params.message,
-        from: '+12037634874',
-        to: request.params.number
+        body: message,
+        from: FROM,
+        to: phoneNumber
     })
     .then(message => console.log(message.sid));
+}
+
+Parse.Cloud.define("sendSMS", function(request, response) {
+    sendSMS(request.params.number, request.params.message);
 });
 
 Parse.Cloud.define("getSubscriberCount", async request=> {
